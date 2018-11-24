6b2f846e50665f83ebb3fb63a900842a35853af0
Yuta <y.nakagawa1107@gmail.com>
2018-11-24 22:37:13 +0900

    できた!が難しい。。。

--- address_seacher.py
+++ address_seacher.py
@@ -5,13 +5,16 @@ class AddressSeacher:
     def __init__(self):
         self.base_url = "http://zipcloud.ibsnet.co.jp/api/search"
 
-    def search(self, postal_code: object) -> object:
+    def search(self, postal_code):
         url = f"{self.base_url}?zipcode={postal_code}"
 
         response = requests.get(url)
 
         response_dict = response.json()
-        都道府県 = response_dict["results"][0]["address1"]
-        市区町村 = response_dict["results"][0]["address2"]
-        町域 = response_dict["results"][0]["address3"]
-        return f"{都道府県}{市区町村}{町域}"
+        if response_dict["results"] is None:
+            return "この郵便番号は存在しません"
+        else:
+            都道府県 = response_dict["results"][0]["address1"]
+            市区町村 = response_dict["results"][0]["address2"]
+            町域 = response_dict["results"][0]["address3"]
+            return f"{都道府県}{市区町村}{町域}"
--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from address_seacher import AddressSeacher
 
 def main():
     # ユーザーからの郵便番号を受け取る
-    postal_code = input("郵便番号を入力してください例1110074　入力:")
     # 郵便番号を使って地名をとってくる
-    address_sercher = AddressSeacher()
-
-    location = address_sercher.search(postal_code)
     # #地名をプリントする
+
+    postal_code = input("郵便番号を入力してください例1110074　入力:")
+    address_sercher = AddressSeacher()
+    location = address_sercher.search(postal_code)
     print(location)
+
 
 
 if __name__ == "__main__":
