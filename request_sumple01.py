a07a0078bedfa36b05a5b58f56b97930ac295ac1
Yuta <y.nakagawa1107@gmail.com>
2018-11-24 15:44:39 +0900

    requestsで勉強中1

--- request_sumple01.py
+++ request_sumple01.py
@@ -1,3 +1,12 @@
 import requests
 
-print(requests)
+岩手県八幡平市大更の情報をrequestsモジュールを使って取得してね。
+response = requests.get(url="http://zipcloud.ibsnet.co.jp/api/search?zipcode=0287111")
+
+print(response)
+
+print(response.text)
+# 引っ張ってくる
+
+別の郵便番号で試す。
+response = requests.get(url="http://zipcloud.ibsnet.co.jp/api/search?zipcode=1130007")
