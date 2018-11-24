2563c6ba0021b836fc44e3174f22de4b22775320
Yuta <y.nakagawa1107@gmail.com>
2018-11-24 17:31:17 +0900

    テストファイルを分ける

--- test_address_seacher.py
+++ test_address_seacher.py
@@ -1,16 +1,6 @@
 import unittest
 
-import requests
-
-class AddressSeacher:
-    def search(self, postal_code):
-        url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=0287111"
-
-        response = requests.get(url)
-        都道府県 = response.json()["results"][0]["address1"]
-        市区町村 = response.json()["results"][0]["address2"]
-        町域 = response.json()["results"][0]["address3"]
-        return f"{都道府県}{市区町村}{町域}"
+from address_seacher import AddressSeacher
 
 
 class TestAddressSeacher(unittest.TestCase):
@@ -27,13 +17,6 @@ class TestAddressSeacher(unittest.TestCase):
         actual = address_sercher.search(postal_code="1760014")
 
         self.assertEqual("東京都練馬区豊玉南", actual)
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
