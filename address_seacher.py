78126049d3e3381bcaf17349bd9710ae7a1d9f93
Yuta <y.nakagawa1107@gmail.com>
2018-11-24 17:42:04 +0900

    ファイル、リファクタリングをした!

---
+++ address_seacher.py
@@ -0,0 +1,17 @@
+import requests
+
+
+class AddressSeacher:
+    def __init__(self):
+        self.base_url = "http://zipcloud.ibsnet.co.jp/api/search"
+
+    def search(self, postal_code):
+        url = f"{self.base_url}?zipcode={postal_code}"
+
+        response = requests.get(url)
+
+        response_dict = response.json()
+        都道府県 = response_dict["results"][0]["address1"]
+        市区町村 = response_dict["results"][0]["address2"]
+        町域 = response_dict["results"][0]["address3"]
+        return f"{都道府県}{市区町村}{町域}"
