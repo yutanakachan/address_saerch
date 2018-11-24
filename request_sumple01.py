153793132edb346940f366bd8458702ab22e50a5
Yuta <y.nakagawa1107@gmail.com>
2018-11-24 15:53:43 +0900

    テストの雛形作成

--- request_sumple01.py
+++ request_sumple01.py
@@ -10,3 +10,30 @@ print(response.text)
 
 別の郵便番号で試す。
 response = requests.get(url="http://zipcloud.ibsnet.co.jp/api/search?zipcode=1130007")
+
+以下コンソールにて
+# import requests
+# response = requests.get(url="http://zipcloud.ibsnet.co.jp/api/search?zipcode=0287111")
+# print(response.text)
+# {
+# 	"message": null,
+# 	"results": [
+# 		{
+# 			"address1": "岩手県",
+# 			"address2": "八幡平市",
+# 			"address3": "大更",
+# 			"kana1": "ｲﾜﾃｹﾝ",
+# 			"kana2": "ﾊﾁﾏﾝﾀｲｼ",
+# 			"kana3": "ｵｵﾌﾞｹ",
+# 			"prefcode": "3",
+# 			"zipcode": "0287111"
+# 		}
+# 	],
+# 	"status": 200
+# }
+# date = response.json()
+# type(date)
+# <class 'dict'>
+
+# print(date["results"][0]["address1"])
+# 岩手県
